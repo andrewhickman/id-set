768e5de31fd4aaab75dc1d75892c3f389dc2960d
Andrew Hickman <andrew.hickman1@sky.com>
2017-06-18 19:05:14 +0100

    update to work with id-map

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(test)] mod tests;
 
+use std::fmt;
 use std::iter::FromIterator;
 
 pub type Id = usize;
@@ -7,12 +8,12 @@ pub type Id = usize;
 type Block = u32;
 const BITS: usize = 32;
 
-/// Given return the smallest integer m such that m*k <= n
-fn num_blocks(bits: usize) -> usize {
-    if bits % BITS == 0 { 
-        bits / BITS
-    } else { 
-        bits / BITS + 1 
+/// Given n and k return the largest integer m such that m*k <= n
+fn ceil_div(n: usize, k: usize) -> usize {
+    if n % k == 0 {
+        n / k
+    } else {
+        n / k + 1
     }
 }
 
@@ -29,15 +30,47 @@ impl IdSet {
         }
     }
 
+    pub fn new_filled(len: usize) -> Self {
+        let (nwords, nbits) = (len / BITS, len % BITS);
+        let mut storage = vec![!0; nwords];
+        if nbits != 0 {
+            storage.push((1u32 << nbits) - 1);
+        }
+        IdSet {
+            storage,
+            len,
+        }
+    }
+
     pub fn with_capacity(nbits: usize) -> Self {
         IdSet {
-            storage: Vec::with_capacity(num_blocks(nbits)),
+            storage: Vec::with_capacity(ceil_div(nbits, BITS)),
             len: 0,
         }
     }
 
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let mut storage = Vec::with_capacity(ceil_div(bytes.len(), 8));
+        let mut len = 0;
+        for chunk in bytes.chunks(4) {
+            let mut word = 0;
+            for &byte in chunk {
+                word <<= 8;
+                word |= byte as u32;
+            }
+            len += word.count_ones() as usize;
+            storage.push(word);
+        }
+        IdSet { storage, len }
+    }
+
     pub fn len(&self) -> usize {
         self.len
+    }
+
+    pub fn clear(&mut self) {
+        self.storage.clear();
+        self.len = 0;
     }
 
     pub fn insert(&mut self, id: Id) -> bool {
@@ -97,6 +130,71 @@ impl IdSet {
     }
 }
 
+impl Clone for IdSet {
+    fn clone(&self) -> Self {
+        IdSet {
+            storage: self.storage.clone(),
+            len: self.len,
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.storage.clone_from(&source.storage);
+        self.len = source.len;
+    }
+}
+
+impl fmt::Debug for IdSet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{")?;
+        let mut iter = self.iter();
+        if let Some(id) = iter.next() {
+            write!(f, "{:?}", id)?;
+            for id in iter {
+                write!(f, ", {:?}", id)?;
+            }
+        }
+        write!(f, "}}")
+    }
+}
+
+impl Default for IdSet {
+    fn default() -> Self {
+        IdSet::new()
+    }
+}
+
+impl Eq for IdSet {}
+
+impl PartialEq for IdSet {
+    fn eq(&self, other: &Self) -> bool {
+        if self.len != other.len {
+            return false;
+        }
+        let (mut lhs, mut rhs) = (self.storage.iter(), other.storage.iter());
+        loop {
+            match (lhs.next(), rhs.next()) {
+                (Some(&l), Some(&r)) => {
+                    if l != r {
+                        return false;
+                    }
+                },
+                (None, None) => return true,
+                (Some(&l), None) => return l == 0 && lhs.all(|&word| word == 0),
+                (None, Some(&r)) => return r == 0 && rhs.all(|&word| word == 0),
+            }
+        }
+    }
+}
+
+impl Extend<Id> for IdSet {
+    fn extend<I: IntoIterator<Item = Id>>(&mut self, iter: I) {
+        for id in iter {
+            self.insert(id);
+        }
+    }
+}
+
 impl FromIterator<Id> for IdSet {
     fn from_iter<I: IntoIterator<Item = Id>>(iter: I) -> Self {
         let mut set = IdSet::new();
@@ -116,6 +214,7 @@ impl<'a> IntoIterator for &'a IdSet {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Iter<'a> {
     storage: &'a [u32],
     word: usize,
@@ -128,9 +227,15 @@ impl<'a> Iterator for Iter<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             if self.bit == BITS {
-                self.word += 1;
-                if self.word >= self.storage.len() {
-                    return None;
+                loop {
+                    if self.word + 1 == self.storage.len() {
+                        return None
+                    }
+                    self.word += 1;
+
+                    if self.storage[self.word] != 0 {
+                        break;
+                    }
                 }
                 self.bit = 0;
             }
@@ -140,5 +245,9 @@ impl<'a> Iterator for Iter<'a> {
                 return Some(self.word * BITS + bit)
             }
         }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some((self.storage.len() - self.word) * BITS - self.bit))
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -32,7 +32,7 @@ fn remove() {
 } 
 
 #[test]
-fn test_bit_set_iterator() {
+fn iterator() {
     let set: IdSet = vec![0, 2, 2, 3].into_iter().collect();
 
     let ids: Vec<Id> = set.iter().collect();
@@ -43,4 +43,56 @@ fn test_bit_set_iterator() {
 
     let idxs: Vec<_> = long.iter().collect();
     assert_eq!(idxs, real);
+}
+
+#[test]
+fn filled() {
+    for n in 0..100 {
+        let set = IdSet::new_filled(n);
+        for k in 0..n {
+            assert!(set.contains(k));
+        }
+        assert!(!set.contains(n));
+    }
+}
+
+#[test]
+fn clone() {
+    let mut a = IdSet::new();
+
+    assert!(a.insert(1));
+    assert!(a.insert(100));
+    assert!(a.insert(1000));
+
+    let mut b = a.clone();
+
+    assert!(a == b);
+
+    assert!(b.remove(1));
+    assert!(a.contains(1));
+
+    assert!(a.remove(1000));
+    assert!(b.contains(1000));
+}
+
+#[test]
+fn eq() {
+    let a = IdSet::from_bytes(&[0b10100010]);
+    let b = IdSet::from_bytes(&[0b00000000]);
+    let c = IdSet::new();
+
+    assert!(a == a);
+    assert!(a != b);
+    assert!(a != c);
+    assert!(b == b);
+    assert!(b == c);
+    assert!(c == c);
+}
+
+#[test]
+fn from_bytes() {
+    let a = IdSet::from_bytes(&[0b01101001, 0b10010110, 0b10010110, 0b01101001]);
+    let b: IdSet = (0usize..32usize).filter(|&n| n.count_ones() % 2 == 0).collect();
+
+    assert!(a == b);
 }
